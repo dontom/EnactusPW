c08db7b73acedf23076b5379a539246e71d76898
Dontom <marcinczykt@gmail.com>
2016-03-09 22:29:13 +0100

    EnactusPW

--- js/contact_me.js
+++ js/contact_me.js
@@ -9,8 +9,8 @@ $(function() {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
             var name = $("input#name").val();
+            var surname = $("input#surname").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -18,11 +18,11 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "marcinczyk3107@gmail.com",
+                url: "../mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
+                    surname: surname,
                     email: email,
                     message: message
                 },
